52fd2aafa06a469d659bb18426bf84bd1b1a93a5
NoListen <listenonepiece@gmail.com>
2018-08-13 15:51:34 +0100

    modify test

--- atari/vae_test.py
+++ atari/vae_test.py
@@ -30,11 +30,15 @@ vae = ConvVAE(name="conv_vae",
               z_size=z_size,
               batch_size=1)
 
+x = tf.placeholder(tf.float32, shape=[None, 64, 64, 1], name="x")
+z = vae.build_encoder(x)
+y = vae.build_decoder(z)
+
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-tf_r_loss = tf.reduce_mean(-tf.reduce_sum(vae.x * tf.log(vae.y+1e-8) +
-                                      (1-vae.x) * (tf.log(1-vae.y+1e-8)),[1,2,3]))
+tf_r_loss = tf.reduce_mean(-tf.reduce_sum(x * tf.log(y+1e-8) +
+                                      (1-x) * (tf.log(1-y+1e-8)),[1,2,3]))
 
 loadFromFlat(vae.get_variables(), 'tf_vae/final_vae.p')
 
@@ -46,8 +50,8 @@ print(n, "images loaded")
 for i in range(n):
     # input
     frame = obs[i].reshape(1, 64, 64, 1)
-    feed = {vae.x: frame}
-    reconstruct, r_loss = sess.run([vae.y, tf_r_loss], feed)
+    feed = {x: frame}
+    reconstruct, r_loss = sess.run([y, tf_r_loss], feed)
     r_losses.append(r_loss)
     #print(i, np.max(frame), np.max(reconstruct), r_loss)
     imsave(output_dir+'/%s.png' % pad_num(i), 255.*frame[0].reshape(64, 64))
